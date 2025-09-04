94fab9fc88f23428a683c946d351daee01be683b
TaghiyevMahammad <tagiyevmahammad0@gmail.com>
2025-09-04 10:45:36 +0400

    Group Anagrams

---
+++ Group_Anagrams/group_anagrams2.js
@@ -0,0 +1,13 @@
+var groupAnagrams = function(strs) {
+      const map = new Map();
+
+  for (let word of strs) {
+    const key = word.split('').sort().join('');
+    if (!map.has(key)) {
+      map.set(key, []);
+    }
+    map.get(key).push(word);
+  }
+
+  return Array.from(map.values());
+}
